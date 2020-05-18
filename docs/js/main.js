01f35af76ac793e5b25895a2794e18a4052af534
Vladimir <watch_dogs1213@mail.ru>
2020-05-18 10:07:26 +0300

    add portfolio

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,4 +12,28 @@ $(document).ready(function () {
     MobileMenuToggle.classList.remove("active");
     MobOverlay.classList.remove("active");
   });
+  let containerEl = document.querySelector("#portfolio-project");
+  let mixer = mixitup(containerEl, {
+    classNames: {
+      block: "",
+    },
+  });
+
+  const filter = document.querySelectorAll(".list-portfolio button");
+  const portfolioBigCard = document.querySelectorAll(".block-portfolio");
+  for (let i = 0; i < filter.length; i++) {
+    filter[i].addEventListener("click", function () {
+      if (i == 0) {
+        for (let j = 0; j < 2; j++) {
+          portfolioBigCard[j].classList.add("block-portfolio-big");
+          portfolioBigCard[j].classList.remove("block-portfolio-small");
+        }
+      } else {
+        for (let j = 0; j < 2; j++) {
+          portfolioBigCard[j].classList.remove("block-portfolio-big");
+          portfolioBigCard[j].classList.add("block-portfolio-small");
+        }
+      }
+    });
+  }
 });
